2310d897b0f7be75e451fb99471fa4adba152b43
shubham0x3f <112776695+ShubhamNegi4@users.noreply.github.com>
2026-07-30 04:17:58 +0530

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+const long long inflong = 0x3f3f3f3f3f3f3f;
+const int inf = 0x3f3f3f3f;
+
+static const int _ = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+/*
+ 
+ 
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if(head == nullptr)return 0;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast -> next != nullptr and fast->next->next != nullptr){
+            slow= slow->next;
+            fast = fast->next->next;
+            if(slow == fast)return 1;
+        }
+        return 0;
+    }
+};
